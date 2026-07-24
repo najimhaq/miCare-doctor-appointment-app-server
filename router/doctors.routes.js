b43ac94937c93b15af5b58a8e2cf6122f99097fc
najimhaq <mdnajimulhaque@gmail.com>
2026-07-24 21:04:09 +0900

    route problem solve

--- router/doctors.routes.js
+++ router/doctors.routes.js
@@ -2,11 +2,15 @@ import express from 'express';
 
 const doctorRouter = express.Router();
 
-import { getDoctors, getDoctorById } from '../controller/doctorsController.js';
+import {
+  getDoctors,
+  getDoctorById,
+  getDoctorSpecialties,
+} from '../controller/doctorsController.js';
 
+doctorRouter.get('/specialties', getDoctorSpecialties);
 doctorRouter.get('/', getDoctors);
 doctorRouter.get('/:id', getDoctorById);
-doctorRouter.get('/specialties', getDoctorSpecialties);
 
 
 export default doctorRouter;
